fe107d25ac8da2091ea2905ee44b7ebd4b5a1002
wenhua.huang <wenhua.huang@aifenlei.com>
2023-03-12 11:48:37 +0800

    基础语法-缩进

---
+++ indentation.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    if True:
+        print("true")
+    else:
+        print("false")
+    print("anywhere")
+multiline_statement
+
--- main.py
+++ main.py
@@ -2,8 +2,19 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+"""
+第三注释
+第四注释
+"""
 
+"""
+第五注释
+第六注释
+"""
 
+"""
+注释
+"""
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.

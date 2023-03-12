723a5e01cdb25551c4cf51554310a6f9ff0c6852
wenhua.huang <wenhua.huang@aifenlei.com>
2023-03-12 18:50:59 +0800

    基础语法- 基础数据类型

---
+++ BasisClass.py
@@ -0,0 +1,16 @@
+# 基础数据类型
+if __name__ == '__main__':
+    counter = 100
+    miles = 1000.0
+    name = 'runoob'
+    print(counter)
+    print(miles)
+    print(name)
+    a = b = c = 1
+    print(a)
+    print(b)
+    print(c)
+    d, e, f = 1, 2, 'runoob'
+    print(d)
+    print(e)
+    print(f)
---
+++ NumberClass.py
@@ -0,0 +1,10 @@
+# 数字类型
+if __name__ == '__main__':
+    a, b, c, d = 20, 5.5, True, 4 + 3j
+    print(type(a))
+    print(type(b))
+    print(type(c))
+    print(type(d))
+    print(isinstance(a, int))
+#     isinstance 认为子类是父类
+#     type

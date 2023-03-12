fbbad034247086a45bde38b5dcf0bd8d37a54755
wenhua.huang <wenhua.huang@aifenlei.com>
2023-03-12 20:57:58 +0800

    基础语法- 字典类型

---
+++ DictClass.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    dict = {}
+    dict['one'] = "1 - 菜鸟教程"
+    dict[2] = "2 - 菜鸟工具"
+    tinydict = {'name': 'runoob', 'code': 1, 'site': 'www.runoob.com'}
+print (dict['one'])       # 1 - 菜鸟教程
+print (dict[2])           # 2 - 菜鸟工具
+print (tinydict)          # {'name': 'runoob', 'code': 1, 'site': 'www.runoob.com'}
+print (tinydict.keys())   # dict_keys(['name', 'code', 'site'])
+print (tinydict.values()) # dict_values(['runoob', 1, 'www.runoob.com'])
+ # a  = dict([('Runoob', 1), ('Google', 2), ('Taobao', 3)])
+

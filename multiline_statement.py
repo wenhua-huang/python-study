ea36ef0f1fbc19d4d73b88714d241a3e740a9791
wenhua.huang <wenhua.huang@aifenlei.com>
2023-03-12 11:50:46 +0800

    基础语法-多行语法

---
+++ multiline_statement.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    total = ['item_one', 'item_two', 'item_three',
+             'item_four']
+    print(total)
+    # Python 通常是一行写完一条语句，但如果语句很长，我们可以使用反斜杠 \ 来实现多行语句
+    sum = 1 + \
+          2 + \
+          3
+    print(sum)

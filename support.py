7009875aed5dffd8a41d5780cbf659dbc50b7b1a
wenhua.huang <wenhua.huang@aifenlei.com>
2023-04-18 17:54:27 +0800

    List

---
+++ support.py
@@ -0,0 +1,6 @@
+def print_func(par):
+    print("Hello : ", par)
+if __name__ == '__main__':
+    print("程序自身在运行support")
+else:
+    print("我来自另一模块")
---
+++ test.py
@@ -0,0 +1,7 @@
+import support
+
+support.print_func("Runoob")
+if __name__ == '__main__':
+    print("程序自身在运行test")
+else:
+    print("我来自另一模块")
